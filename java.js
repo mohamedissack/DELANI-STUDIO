c8d7440bd89572b402b17cbfd3271883b7017c07
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-09-13 07:25:35 +0300

    add js functionality to the portfolio

--- java.js
+++ java.js
@@ -31,4 +31,69 @@ $(".imag").click(function(){
 
 });
 
+//overlay 4
+$("#work4")
+  .mouseenter(function() {
+    $("#overlay4").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay4").hide("slow");
+  });
+
+//overlay3
+  $("#work3")
+  .mouseenter(function() {
+    $("#overlay3").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay3").hide("slow");
+  });
+//overlay2
+  $("#work2")
+  .mouseenter(function() {
+    $("#overlay2").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay2").hide("slow");
+  });
+  //overlay5
+  $("#work5")
+  .mouseenter(function() {
+    $("#overlay5").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay5").hide("slow");
+  });
+  //overlay6
+  $("#work6")
+  .mouseenter(function() {
+    $("#overlay6").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay6").hide("slow");
+  });
+  //overlay7
+  $("#work7")
+  .mouseenter(function() {
+    $("#overlay7").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay7").hide("slow");
+  });
+  //overlay8
+  $("#work8")
+  .mouseenter(function() {
+    $("#overlay8").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay8").hide("slow");
+  });
+  //overlay1
+  $("#work1")
+  .mouseenter(function() {
+    $("#overlay").show("slow");
+  })
+  .mouseleave(function() {
+    $("#overlay").hide("slow");
+  });
 
